fa61d570aa43b3f4fda43f9d791af8146090c575
Jeong Inhyeok <inhyeok0327@gmail.com>
2016-07-23 23:25:09 +0900

    Fix side user list

--- static/js/main.js
+++ static/js/main.js
@@ -2,15 +2,12 @@ $(function () {
   __ = $('.side-label')
   __.on('click', function () {
     $side = $('.side')
-    open = $side.data('side-open')
-
+    open = $side.hasClass('open')
     if(open == false) {
       $('.side').addClass('open')
     }
     else if(open == true) {
       $('.side').removeClass('open')
     }
-
-    $side[0].setAttribute('data-side-open', !open)
   })
 });
