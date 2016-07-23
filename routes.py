442d893e412fee43726990fc1ea2b0192e0f05a2
Jeong Inhyeok <inhyeok0327@gmail.com>
2016-07-23 22:41:40 +0900

    Fix table name

--- routes.py
+++ routes.py
@@ -10,7 +10,7 @@ metadata = MetaData(bind=db)
 
 app.secret_key = 'I12am34ITS56by78Enon'
 
-Users = Table('users', metadata, autoload=True)
+UserList = Table('user_list', metadata, autoload=True)
 GroupList = Table('group_list', metadata, autoload=True)
 EventList = Table('event_list', metadata, autoload=True)
 
@@ -27,19 +27,19 @@ def signin():
   if request.method == 'POST':
     id = request.form['id']
     pw = request.form['pw']
-    user = Users.select(Users.c.id == id).execute().first()
+    user = UserList.select(UserList.c.id == id).execute().first()
     if user:
       hash_pw = hashlib.sha1(pw+'enon').hexdigest()
       if user.pw == hash_pw:
-        group_users = []
+        group_user_list = []
         session['user'] = dict(user)
         group = GroupList.select(GroupList.c.id == user.group_id).execute().first()
         if group:
           session['group'] = dict(group)
-          group_users_result = Users.select(Users.c.group_id == user.group_id).execute()
-          for item in group_users_result:
-            group_users.append(dict(item))
-          session['group_users'] = group_users
+          result = UserList.select(UserList.c.group_id == user.group_id).execute()
+          for item in result:
+            group_user_list.append(dict(item))
+          session['group_user_list'] = group_user_list
         else:
           session['group'] = {}
         return jsonify({'status': 200, 'message': 'success'})
@@ -53,7 +53,7 @@ def signin():
 def logout():
   session.pop('user', None)
   session.pop('group', None)
-  session.pop('group_users', None)
+  session.pop('group_user_list', None)
   return redirect(url_for('index'))
 
 @app.route('/signup', methods=['GET', 'POST', 'PUT'])
@@ -67,25 +67,25 @@ def signup():
       'group_code': request.form['group_code'] or '',
       'group_id': 0
     }
-    result = Users.select(Users.c.id == user['id']).execute().first()
+    result = UserList.select(UserList.c.id == user['id']).execute().first()
     if result:
       return jsonify({'status': 204, 'message': '이미 사용중인 아이디입니다.'})
     group_code_check = GroupList.select(GroupList.c.code == user['group_code']).execute().first()
     if group_code_check:
       user['group_id'] = group_code_check['id']
     user['pw'] = hashlib.sha1(user['pw']+'enon').hexdigest()
-    Users.insert().execute(user)
+    UserList.insert().execute(user)
 
-    # group_users = []
+    # group_user_list = []
     # session['user'] = dict(user)
 
     # # Add user session
     # if user['group_id'] != 0:
     #   session['group'] = dict(group_code_check)
-    #   group_users = Users.select(Users.c.group_id == user['group_id']).execute()
-    #   for item in group_users:
-    #     group_users.append(dict(item))
-    #   session['group_users'] = group_users
+    #   result = UserList.select(UserList.c.group_id == user['group_id']).execute()
+    #   for item in result:
+    #     group_user_list.append(dict(item))
+    #   session['group_user_list'] = group_user_list
     # else:
     #   session['group'] = {}
 
@@ -151,12 +151,12 @@ def group():
         return jsonify({'status': 204, 'message': '이미 사용중인 그룹이름입니다.'})
       GroupList.insert().execute(name=group_name, code=group_code)
       group_info = GroupList.select(GroupList.c.code == group_code).execute().first()
-      Users.update(Users.c.code == session['user']['code']).execute(group_id=group_info['id'])
+      UserList.update(UserList.c.code == session['user']['code']).execute(group_id=group_info['id'])
     except Exception, e:
       return jsonify({'status': 500, 'message': e})
     session['user']['group_id'] = group_info['id']
     session['group'] = dict(group_info)
-    session['group_users'] = [session['user']]
+    session['group_user_list'] = [session['user']]
     return jsonify({'status': 200, 'message': '성공'})
 
   elif request.method == 'PUT':
